a5f5fbddf5433fedfd5bf85eaa5e1131c451b490
amit <amitchakraborty637@gmail.com>
2021-04-10 15:01:12 +0530

    firestore data insert in kotlin

--- app/src/main/java/com/example/firestrore_data_insert/MainActivity.kt
+++ app/src/main/java/com/example/firestrore_data_insert/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.firestrore_data_insert
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +15,19 @@ class MainActivity : AppCompatActivity() {
             var name:String=name.text.toString()
             var marks:Int=marks.text.toString().toInt()
 
+val db=FirebaseFirestore.getInstance()
+
+            val user:MutableMap<String, Any> =HashMap()
+            user["name"]=name
+            user["marks"]=marks
+            db.collection("users")
+                .add(user)
+                .addOnSuccessListener {
+                    Toast.makeText(this,"Data insert successfully",Toast.LENGTH_LONG).show()
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this,"Error",Toast.LENGTH_LONG).show()
+                }
         }
     }
 }
